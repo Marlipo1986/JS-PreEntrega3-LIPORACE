6cb26336a2e9e712caf16d520f4b8c7cb16f64b3
Martin Liporace <marlipo1986@gmail.com>
2023-08-22 15:41:27 -0300

    Subiendo primeras correcciones

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+
+const divPlatos = document.getElementById("platos")
+const divPedidos = document.getElementById("pedidos")
+
+let platosDisponibles = JSON.parse(localStorage.getItem("platos"))
+
+document.addEventListener("DOMContentLoaded",() => {
+    generarCardsPlatos(platosDisponibles)
+})
+
+export const generarCardsPlatos = (platos) => {
+    divPlatos.innerHTML = "";
+    
+    platos.forEach((plato) => {
+
+    const { imagen, nombre, categoria, precio, id} = plato
+
+        let card = document.createElement("div")
+        card.className = "plato"
+        card.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="${imagen}" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">${nombre}</h5>
+            <p class="card-text">${categoria}</p>
+            <p class="card-text">$${precio}</p>
+            <button id="boton${id}" class="btn btn-primary">Comprar</button>
+            </div>
+        </div>`
+        
+        divPlatos.appendChild(card);
+
+        const btnComprar = document.getElementById(`boton${plato.id}`)
+        btnComprar.addEventListener("click", () => console.log("funciona"))
+
+    });
+};
+
